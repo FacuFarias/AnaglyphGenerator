5385140667fce0446d0591da2319b7f5f199cb26
FacuFarias <facufarias93@gmail.com>
2023-06-18 16:04:17 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,234 @@
+# Type YOUR MESSAGE (capitals and numbers only)
+message = "TE AMO"
+
+# Type YOUR MESSAGE (capitals and numbers only)
+name_archivo = "prueba5.png"
+
+# Select difficulty
+difficulty = 0  # 0=easy, 4=ballanced, 10=difficult, 20=impossible, 50=random
+
+# Select size of a pixel
+pixel = 5
+
+# Select the distance between edge of the image and the text
+padding = 14
+
+
+# Symbols graphics
+Ax = [-4, -3, -2, -1, -1, -1, 0, 0, 0, 1, 1, 1, 1]
+Ay = [4, 3, 3, 2, 4, 7, 2, 5, 6, 1, 2, 3, 4]
+Bx = [-4, -3, -3, -2, -2, -1, -1, -1, 0, 0, 0, 1, 1, 2, 2]
+By = [4, 3, 5, 2, 6, 1, 3, 6, 0, 4, 5, 1, 4, 2, 3]
+Cx = [-3, -3, -3, -2, -2, -1, -1, 0, 1, 2, 2]
+Cy = [3, 4, 5, 2, 6, 1, 6, 1, 1, 2, 3]
+Dx = [-4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2]
+Dy = [4, 3, 5, 2, 6, 1, 6, 0, 5, 1, 4, 2, 3]
+Ex = [-4, -3, -3, -2, -2, -1, -1, -1, 0, 0, 1, 2, 3]
+Ey = [4, 3, 5, 2, 6, 1, 3, 7, 0, 4, 1, 2, 3]
+Fx = [-4, -3, -2, -1, -1, 0, 0, 1, 2, 3]
+Fy = [4, 3, 2, 1, 3, 0, 4, 1, 2, 3]
+Gx = [-3, -3, -3, -2, -2, -1, -1, -1, 0, 0, 0, 1, 2, 2]
+Gy = [3, 4, 5, 2, 6, 1, 4, 6, 1, 5, 6, 1, 2, 3]
+Hx=[-4, -3, -2, -2, -1, -1, -1, 0, 0, 1, 2]
+Hy=[ 4,  3,  2,  6,  1,  3,  5, 0, 4, 3, 2]
+Ix=[-4, -3, -2, -1, 0]
+Iy=[ 4,  3,  2,  1, 0]
+Jx=[-3, -3, -3, -2, -1, 0, 0, 1, 1, 2, 2, 3]
+Jy=[ 3,  4,  5,  6,  6, 0, 6, 1, 5, 2, 4, 3]
+Kx=[-4, -3, -2, -2, -1, -1, -1, -1, 0, 0, 1, 2]
+Ky=[ 4,  3,  2,  6,  1,  3,  4,  5, 0, 3, 3, 2]
+Lx=[-4, -3, -3, -2, -2, -1, 0]
+Ly=[ 4,  3,  5,  2,  6,  1, 0]
+Mx=[-4, -3, -2, -1, -1, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3]
+My=[ 4,  3,  2,  1,  7, 0, 1, 2, 3, 6, 3, 5, 3, 4, 3]
+Nx=[-4, -3, -2, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 1, 2, 3]
+Ny=[ 4,  3,  2,  1,  4,  5,  6,  7, 0, 1, 2, 3, 6, 5, 4, 3]
+Ox=[-3, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2, 2]
+Oy=[ 3,  4,  5,  2,  6,  1,  6, 1, 6, 1, 5, 2, 3, 4]
+Px=[-4, -3, -2, -1, -1, 0, 0, 1, 1, 2, 2]
+Py=[ 4,  3,  2,  1,  3, 0, 4, 1, 4, 2, 3]
+Qx=[-3, -3, -3, -2, -2, -1, -1, -1, -1, 0, 0, 1, 1, 2, 2, 2]
+Qy=[ 3,  4,  5,  2,  6,  1,  5,  6,  7, 1, 6, 1, 5, 2, 3, 4]
+Rx=[-4, -3, -2, -1, -1, -1, -1, -1, -1, 0, 0, 1, 1, 2, 2]
+Ry=[ 4,  3,  2,  1,  3,  4,  5,  6,  7, 0, 4, 1, 4, 2, 3]
+Sx=[-3, -3, -2, -1, -1, -1, -1, 0, 0, 0, 0, 1, 2, 2]
+Sy=[ 4,  5,  6,  1,  2,  3,  6, 1, 4, 5, 6, 1, 2, 3]
+Tx=[-3, -2, -1, 0, 0, 1, 2]
+Ty=[ 5,  4,  3, 0, 2, 1, 2]
+Ux=[-3, -3, -3, -2, -2, -1, -1, 0, 0, 1, 2, 3]
+Uy=[ 3,  4,  5,  2,  6,  1,  6, 0, 6, 5, 4, 3]
+Vx=[-2, -2, -2, -2, -1, -1, -1, -1, 0, 0, 1, 2, 3]
+Vy=[ 3,  4,  5,  6,  1,  2,  3,  5, 0, 5, 4, 4, 3]
+Wx=[-3, -3, -2, -2, -1, -1, -1, -1, 0, 0, 0, 1, 2, 3]
+Wy=[ 3,  4,  2,  4,  1,  4,  5,  6, 0, 3, 6, 5, 4, 3]
+Xx=[-4, -3, -3, -2, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 1, 2, 2, 3]
+Xy=[ 4,  3,  4,  3,  1,  2,  3,  6,  7, 0, 1, 4, 5, 6, 4, 3, 4, 3]
+Yx=[-3, -2, -1, 0, 0, 0, 0, 1, 2, 3]
+Yy=[ 5,  4,  4, 0, 1, 2, 3, 3, 3, 3]
+Zx=[-4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2, 3]
+Zy=[ 4,  4,  5,  4,  6,  4,  7, 0, 3, 1, 3, 2, 3, 3]
+n0x=[-4, -3, -3, -3, -2, -2, -2, -1, -1, -1, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3]
+n0y=[ 4,  3,  4,  5,  2,  4,  6,  1,  4,  6, 1, 3, 6, 1, 3, 5, 2, 3, 4, 3]
+n1x=[-3, -2, -1, -1, -1, 0, 0, 1, 1, 2]
+n1y=[ 5,  6,  2,  5,  7, 2, 4, 2, 3, 2]
+n2x=[-4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2]
+n2y=[ 4,  4,  5,  4,  6,  4,  7, 1, 4, 1, 4, 2, 3]
+n3x=[-3, -3, -2, -1, 0, 0, 0, 0, 1, 1, 2, 2, 2]
+n3y=[ 4,  5,  6,  6, 1, 4, 5, 6, 1, 4, 2, 3, 4]
+n4x=[-2, -2, -1, -1, -1, 0, 0, 1, 2]
+n4y=[ 2,  6,  1,  3,  5, 0, 4, 3, 2]
+n5x=[-4, -3, -2, -2, -1, -1, -1, 0, 0, 0, 1, 2]
+n5y=[ 4,  5,  2,  6,  1,  3,  6, 0, 4, 5, 1, 2]
+n6x=[-3, -3, -3, -2, -2, -2, -1, -1, -1, 0, 0, 0, 0, 1, 2, 2]
+n6y=[ 3,  4,  5,  2,  3,  6,  1,  3,  6, 1, 4, 5, 6, 1, 2, 3]
+n7x=[-3, -2, -1, 0, 0, 1, 1, 2]
+n7y=[ 5,  4,  3, 0, 3, 1, 3, 2]
+n8x=[-3, -3, -2, -2, -1, -1, -1, 0, 0, 0, 1, 1, 2, 2]
+n8y=[ 4,  5,  3,  6,  2,  3,  6, 1, 4, 5, 1, 4, 2, 3]
+n9x=[-3, -3, -2, -1, -1, -1, 0, 0, 0, 1, 1, 1, 2, 2, 2]
+n9y=[ 4,  5,  6,  2,  3,  6, 1, 4, 6, 1, 4, 5, 2, 3, 4]
+
+symbol_mapping = {
+    "A": (Ax, Ay),
+    "B": (Bx, By),
+    "C": (Cx, Cy),
+    "D": (Dx, Dy),
+    "E": (Ex, Ey),
+    "F": (Fx, Fy),
+    "G": (Gx, Gy),
+    "H": (Hx, Hy),
+    "I": (Ix, Iy),
+    "J": (Jx, Jy),
+    "K": (Kx, Ky),
+    "L": (Lx, Ly),
+    "M": (Mx, My),
+    "N": (Nx, Ny),
+    "O": (Ox, Oy),
+    "P": (Px, Py),
+    "Q": (Qx, Qy),
+    "R": (Rx, Ry),
+    "S": (Sx, Sy),
+    "T": (Tx, Ty),
+    "U": (Ux, Uy),
+    "V": (Vx, Vy),
+    "W": (Wx, Wy),
+    "X": (Xx, Xy),
+    "Y": (Yx, Yy),
+    "Z": (Zx, Zy),
+    "0": (n0x, n0y),
+    "1": (n1x, n1y),
+    "2": (n2x, n2y),
+    "3": (n3x, n3y),
+    "4": (n4x, n4y),
+    "5": (n5x, n5y),
+    "6": (n6x, n6y),
+    "7": (n7x, n7y),
+    "8": (n8x, n8y),
+    "9": (n9x, n9y),
+    }
+
+import random
+from PIL import Image, ImageDraw
+
+width = 400
+height = 400
+
+# coor intervals for red and cyan spectrum
+redTop = 55
+blueLow = 75
+blueTop = 190
+purpleLow = 200
+
+MessageX = []
+MessageY = []
+
+# initial cursor position
+typingPos = [padding + 4, padding]
+
+def addSymbol(symbolX, symbolY):
+    symbolSize = 0
+    for i in range(len(symbolX)):
+        MessageX.append(symbolX[i] + typingPos[0])
+        MessageY.append(symbolY[i] + typingPos[1])
+        symbolSize = max(symbolSize, symbolX[i] + symbolY[i])
+    typingPos[0] += round(symbolSize/2) + 2
+    typingPos[1] += round(symbolSize/2) + 2
+
+for ch in message:
+    if ch in symbol_mapping:
+        addSymbol(*symbol_mapping[ch])
+    else:
+        typingPos[0] += 4
+        typingPos[1] += 4
+
+
+def selectCyan():
+    global odstin, satur
+    odstin = random.random() * 360
+    #satur = 255
+    satur = random.random() * 255
+    if satur > 1:
+        satur = 255
+    while odstin < blueLow or odstin > blueTop:
+        odstin = random.random() * 360
+
+def selectRed():
+    global odstin, satur
+    satur = random.random() * 255
+    odstin = int(random.random() * 360)
+    while odstin > redTop and odstin < purpleLow:
+        odstin = int(random.random() * 360)
+    satur = int(random.random() * 8)
+    if satur > 1:
+        satur = 255
+
+def hledej(pozX, pozY):
+    for k in range(len(MessageX)):
+        if MessageX[k] == pozX and MessageY[k] == pozY:
+            return True
+    return False
+
+
+pom = 0
+import random
+import colorsys
+image = Image.new("RGB", (width, height),"white")
+global odstin
+global satur
+
+for i in range(int(width/pixel)):
+    for j in range(int(width/pixel)):
+        odstin = int(random.random()*255)
+        satur = 255
+        pom = random.random() * 100
+        #print(i)
+        if hledej(i, j):
+            if pom<difficulty:
+                selectRed()
+            else:
+                selectCyan()       
+            draw = ImageDraw.Draw(image)
+            rgb_color = colorsys.hsv_to_rgb(odstin / 360.0, satur / 255.0, 255 / 255.0)
+            rgb_color = tuple(int(channel * 255) for channel in rgb_color)
+            draw.rectangle((pixel * i, pixel * j, pixel * i + pixel, pixel * j + pixel), fill=(rgb_color))
+        elif (abs(i - j) < 7 and abs(i * pixel + j * pixel - width) < (width - 1.9 * pixel * padding)) or odstin < 155:
+            if pom<difficulty:
+                selectCyan()
+            else:
+                selectRed()
+            #print("second")
+            draw = ImageDraw.Draw(image)
+            rgb_color = colorsys.hsv_to_rgb(odstin / 360.0, satur / 255.0, 255 / 255.0)
+            rgb_color = tuple(int(channel * 255) for channel in rgb_color)
+            draw.rectangle((pixel * i, pixel * j, pixel * i + pixel, pixel * j + pixel), fill=(rgb_color))
+        else:
+            if(pom<50):
+                selectCyan()
+            else:
+                selectRed()
+            draw = ImageDraw.Draw(image)
+            rgb_color = colorsys.hsv_to_rgb(odstin / 360.0, satur / 255.0, 255 / 255.0)
+            rgb_color = tuple(int(channel * 255) for channel in rgb_color)
+            draw.rectangle((pixel * i, pixel * j, pixel * i + pixel, pixel * j + pixel), fill=(rgb_color))  
+        #print(type(satur))
+image.save(name_archivo)
